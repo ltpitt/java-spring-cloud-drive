28313b0a7b1fe752b3235534521d9de8482b38c9
Davide Nastri <d.nastri@gmail.com>
2020-07-21 17:55:17 +0200

    First commit

---
+++ src/main/java/it/davidenastri/clouddrive/CloudDriveApplication.java
@@ -0,0 +1,13 @@
+package it.davidenastri.clouddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudDriveApplication.class, args);
+	}
+
+}
