caaa61394f9deb6d944a7fab9076b82f7c1dfd34
Davide Nastri <d.nastri@gmail.com>
2020-07-22 22:31:27 +0200

    feat: Implemented logout

---
+++ src/main/java/it/davidenastri/clouddrive/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package it.davidenastri.clouddrive.config;
+
+
+import it.davidenastri.clouddrive.services.AuthenticationService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private AuthenticationService authenticationService;
+
+    public SecurityConfig(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(this.authenticationService);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.logout()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                .invalidateHttpSession(true)        // set invalidation state when logout
+                .deleteCookies("JSESSIONID");
+
+        http.authorizeRequests()
+                // Enabling h2-console for development
+                .antMatchers("/css/**", "/js/**", "/signup", "/logout", "/h2-console/**").permitAll()
+                .anyRequest().authenticated();
+
+        // Enabling h2-console for development
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        http.formLogin()
+                .defaultSuccessUrl("/home", true);
+
+    }
+
+}
---
+++ src/main/java/it/davidenastri/clouddrive/controller/HomeController.java
@@ -0,0 +1,15 @@
+package it.davidenastri.clouddrive.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomeController {
+
+    @GetMapping()
+    public String loginView() {
+        return "home";
+    }
+}
