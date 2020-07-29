9c87f441bf8ac50e6739ba8cbd15a172e7bb8e74
Davide Nastri <d.nastri@gmail.com>
2020-07-29 21:39:11 +0200

    bug: Fixed a redirection error that sent the user to login page even if sign up was not successful

--- src/main/java/it/davidenastri/clouddrive/controller/SignupController.java
+++ src/main/java/it/davidenastri/clouddrive/controller/SignupController.java
@@ -41,10 +41,11 @@ public class SignupController {
 
         if (signupError == null) {
             model.addAttribute("signupSuccess", true);
+            return "login";
         } else {
             model.addAttribute("signupError", signupError);
+            return "signup";
         }
 
-        return "login";
     }
 }
